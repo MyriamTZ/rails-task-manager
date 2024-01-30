76c45a4def700499585d2ca649d4e823c099265f
Myriam ZAIBET <myriam.zaibet@gmail.com>
2024-01-30 07:13:24 +0100

    exo rails task manager

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  get "tasks", to: "tasks#index" # voir la liste de toutes les tâches
+  get "tasks/new", to: "tasks#new", as: :new_task # envoyer les données (via un form)
+  post "tasks", to: "tasks#create" # réception des données
+  get "tasks/:id", to: "tasks#show", as: :task # voir le détail d'une seule tâche
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_tasks
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy"
 end
